5d3b3ad7a303d37a368432baaa268eefcd4eb0b5
Igor Morais <mor41s.1gor@gmail.com>
2015-06-05 02:31:45 -0300

    Create stubs for menu views

---
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/SettingsFragment.java
@@ -0,0 +1,21 @@
+package com.epitrack.guardioes.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epitrack.guardioes.R;
+
+/**
+ * Created by IgorMorais on 6/4/15.
+ */
+public class SettingsFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settings_fragment, container, false);
+    }
+}
