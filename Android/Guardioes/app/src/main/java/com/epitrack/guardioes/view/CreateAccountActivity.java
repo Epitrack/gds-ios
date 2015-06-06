b5b2aa5ae667523be7c34b9f6c355cb87eea56c2
Igor Morais <mor41s.1gor@gmail.com>
2015-06-05 23:05:40 -0300

    Add "Create Account Screen"

---
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/CreateAccountActivity.java
@@ -0,0 +1,43 @@
+package com.epitrack.guardioes.view;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.epitrack.guardioes.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class CreateAccountActivity extends BaseActivity {
+
+    @InjectView(R.id.create_account_activity_edit_text_mail)
+    EditText editTextMail;
+
+    @InjectView(R.id.create_account_activity_edit_text_password)
+    EditText editTextPassword;
+
+    @InjectView(R.id.create_account_activity_edit_text_repeat_password)
+    EditText editTextRepeatPassword;
+
+    @InjectView(R.id.create_account_activity_edit_text_name)
+    EditText editTextName;
+
+    @InjectView(R.id.create_account_activity_edit_text_birth_date)
+    EditText editTextBirthDate;
+
+    @Override
+    protected void onCreate(final Bundle bundle) {
+        super.onCreate(bundle);
+
+        setContentView(R.layout.create_account_activity);
+
+        ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.create_account_activity_button_register)
+    public void onEnter(final View view) {
+        navigateTo(MainActivity.class);
+    }
+}
--- Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/CreateAccountScreen.java
+++
@@ -1,7 +0,0 @@
-package com.epitrack.guardioes.view.view;
-
-import com.epitrack.guardioes.view.view.BaseActivity;
-
-public class CreateAccountScreen extends BaseActivity {
-
-}
