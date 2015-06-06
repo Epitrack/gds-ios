e708a238972e49e12ed4b1413b3db0ffadf8e29d
Igor Morais <mor41s.1gor@gmail.com>
2015-06-06 10:40:54 -0300

    Add "Map Screen"

--- Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/MapActivity.java
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/MapActivity.java
@@ -1,5 +1,28 @@
 package com.epitrack.guardioes.view;
 
-public class MapActivity extends BaseActivity {
+import android.os.Bundle;
 
+import com.epitrack.guardioes.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MapActivity extends BaseActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(final Bundle bundle) {
+        super.onCreate(bundle);
+
+        setContentView(R.layout.map_activity);
+
+        final MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map_activity_fragment_map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.setMyLocationEnabled(true);
+    }
 }
