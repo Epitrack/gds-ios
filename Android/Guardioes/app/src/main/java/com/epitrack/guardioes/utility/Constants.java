0159d33e97b08d79e4c59fd0abc1dae71ffcb900
Igor Morais <mor41s.1gor@gmail.com>
2015-06-09 22:17:45 -0300

    Add GCM push support

---
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/utility/Constants.java
@@ -0,0 +1,16 @@
+package com.epitrack.guardioes.utility;
+
+/**
+ * Created by IgorMorais on 6/9/15.
+ */
+public final class Constants {
+
+    private Constants() {
+
+    }
+
+    public static final class Push {
+
+        public static final String SENDER = "1008511817333";
+    }
+}
