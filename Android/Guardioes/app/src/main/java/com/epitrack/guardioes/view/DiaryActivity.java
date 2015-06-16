4122b490add6cf39f49b266df739e048cf7a8672
Igor Morais <mor41s.1gor@gmail.com>
2015-06-16 20:44:06 -0300

    Add some stubs classes

--- Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/DiaryActivity.java
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/DiaryActivity.java
@@ -1,5 +1,15 @@
 package com.epitrack.guardioes.view;
 
+import android.os.Bundle;
+
+import com.epitrack.guardioes.R;
+
 public class DiaryActivity extends BaseAppCompatActivity {
 
+    @Override
+    protected void onCreate(final Bundle bundle) {
+        super.onCreate(bundle);
+
+        setContentView(R.layout.diary_activity);
+    }
 }
--- Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/TipActivity.java
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/TipActivity.java
@@ -2,10 +2,14 @@ package com.epitrack.guardioes.view;
 
 import android.os.Bundle;
 
+import com.epitrack.guardioes.R;
+
 public class TipActivity extends BaseAppCompatActivity {
 
     @Override
     protected void onCreate(final Bundle bundle) {
         super.onCreate(bundle);
+
+        setContentView(R.layout.tip_activity);
     }
 }
