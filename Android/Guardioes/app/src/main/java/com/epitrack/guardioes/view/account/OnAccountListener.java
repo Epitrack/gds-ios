2220c0215bf328fd679ec56da13ae15bc5a2be03
Igor Morais <mor41s.1gor@gmail.com>
2015-06-19 19:50:07 -0300

    Add generic interface to support login with social networks

---
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/account/OnAccountListener.java
@@ -0,0 +1,13 @@
+package com.epitrack.guardioes.view.account;
+
+/**
+ * Created by IgorMorais on 6/19/15.
+ */
+public interface OnAccountListener {
+
+    void onError();
+
+    void onCancel();
+
+    void onSuccess();
+}
