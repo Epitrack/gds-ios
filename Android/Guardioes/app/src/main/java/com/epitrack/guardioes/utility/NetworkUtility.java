e1a1252068aaebd991b60da93c18b67893821938
Igor Morais <mor41s.1gor@gmail.com>
2015-06-19 19:47:27 -0300

    Add stubs

---
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/utility/NetworkUtility.java
@@ -0,0 +1,10 @@
+package com.epitrack.guardioes.utility;
+
+public final class NetworkUtility {
+
+    private static final String TAG = Utility.class.getSimpleName();
+
+    private NetworkUtility() {
+
+    }
+}
---
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/utility/Utility.java
@@ -0,0 +1,10 @@
+package com.epitrack.guardioes.utility;
+
+public final class Utility {
+
+    private static final String TAG = Utility.class.getSimpleName();
+
+    private Utility() {
+
+    }
+}
