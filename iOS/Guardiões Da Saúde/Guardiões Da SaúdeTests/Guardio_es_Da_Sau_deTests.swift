174298a0d3cc738d61d7b4c494f20c47550f63e2
Igor Morais <mor41s.1gor@gmail.com>
2015-06-23 19:13:56 -0300

    Initial commit (iOS)

---
+++ iOS/Guardiões Da Saúde/Guardiões Da SaúdeTests/Guardio_es_Da_Sau_deTests.swift
@@ -0,0 +1,36 @@
+//
+//  Guardio_es_Da_Sau_deTests.swift
+//  Guardiões Da SaúdeTests
+//
+//  Created by Igor Morais on 6/15/15.
+//  Copyright (c) 2015 Epitrack. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Guardio_es_Da_Sau_deTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
