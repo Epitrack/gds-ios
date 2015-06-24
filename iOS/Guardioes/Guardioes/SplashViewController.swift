eb0b26e5876d09f530c77115a91ce950676d6f9d
Igor Morais <mor41s.1gor@gmail.com>
2015-06-24 20:12:01 -0300

    Add some stubs

---
+++ iOS/Guardioes/Guardioes/SplashViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SplashViewController.swift
+//  Guardioes
+//
+//  Created by Igor Morais on 6/24/15.
+//  Copyright (c) 2015 Epitrack. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: NSObject {
+   
+}
