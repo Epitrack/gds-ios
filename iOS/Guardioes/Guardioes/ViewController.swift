7398403f54ed68d90fb46773bfdbb14ffa0a85a8
Igor Morais <mor41s.1gor@gmail.com>
2015-06-24 10:46:49 -0300

    Add iOS project

---
+++ iOS/Guardioes/Guardioes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guardioes
+//
+//  Created by Igor Morais on 6/24/15.
+//  Copyright (c) 2015 Epitrack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
