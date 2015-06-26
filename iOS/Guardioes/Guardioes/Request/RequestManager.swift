85697f5692bd0d12a1d930c85d7f0fe4399e8219
Igor Morais <mor41s.1gor@gmail.com>
2015-06-26 17:31:47 -0300

    Add request wrapper

--- iOS/Guardioes/Guardioes/Request/RequestManager.swift
+++ iOS/Guardioes/Guardioes/Request/RequestManager.swift
@@ -1,34 +1,134 @@
 import Alamofire
+import OCMapper
 
 class RequestManager {
     
-    func a() {
-        Alamofire.request(.GET, "http://httpbin.org/get")
-            .responseJSON { (_, _, JSON, _) in
-                println(JSON)
+    func request(method: Alamofire.Method,
+                 url: URLStringConvertible,
+                 onError: (NSError) -> Void,
+                 onSuccess: (String) -> Void) {
+                    
+        Alamofire.request(method, url, encoding: .JSON)
+                 .validate()
+                 .responseJSON { (request, response, json, error) in
+                    
+            if let json: AnyObject = json {
+                onSuccess(json as! String)
+            
+            } else if let error = error {
+                onError(error)
+            }
         }
     }
-//    
-//    func request(method: Method, url: URLStringConvertible, parameterMap: [String : AnyObject]? = nil, encoding: ParameterEncoding = .URL) {
-//        
-//        Alamofire.request(method, url, parameters: parameterMap, encoding: encoding).validate().responseJSON(options: <#NSJSONReadingOptions#>, completionHandler: <#(NSURLRequest, NSHTTPURLResponse?, AnyObject?, NSError?) -> Void##(NSURLRequest, NSHTTPURLResponse?, AnyObject?, NSError?) -> Void#>)
-//    }completionHandler: (NSURLRequest, NSHTTPURLResponse?, AnyObject?, NSError?) -> Void)
     
-    func request <T: NSObject> (method: Method, url: URLStringConvertible, parameterMap: [String : AnyObject]? = nil, encoding: ParameterEncoding = .URL
-        , onError: (NSError), onSuccess: (T)) {
-        
+    func request(method: Alamofire.Method,
+                 url: URLStringConvertible,
+                 parameterMap: [String : AnyObject],
+                 onError: (NSError) -> Void,
+                 onSuccess: (String) -> Void) {
+                    
+        Alamofire.request(method, url, parameters: parameterMap, encoding: .JSON)
+                 .validate()
+                 .responseJSON { (request, response, json, error) in
+                    
+            if let json: AnyObject = json {
+                onSuccess(json as! String)
+                        
+            } else if let error = error {
+                onError(error)
+            }
+        }
+    }
+    
+    func request(method: Alamofire.Method,
+                 url: URLStringConvertible,
+                 parameterMap: [String : AnyObject],
+                 encoding: ParameterEncoding,
+                 onError: (NSError) -> Void,
+                 onSuccess: (String) -> Void) {
+                    
         Alamofire.request(method, url, parameters: parameterMap, encoding: encoding)
                  .validate()
                  .responseJSON { (request, response, json, error) in
                     
-                    if let error = error {
-                        
-                        onError(error)
-                        
-                    } else {
-                        
-                    }
+            if let json: AnyObject = json {
+                onSuccess(json as! String)
+            
+            } else if let error = error {
+                onError(error)
+            }
         }
     }
     
+    func request <Type: NSObject> (method: Alamofire.Method,
+                                   url: URLStringConvertible,
+                                   type: Type.Type,
+                                   onError: (NSError) -> Void,
+                                   onSuccess: (Type) -> Void) {
+                                    
+        Alamofire.request(method, url, encoding: .JSON)
+                 .validate()
+                 .responseJSON { (request, response, json, error) in
+                    
+            if let json: AnyObject = json {
+                
+                let entity = ObjectMapper.sharedInstance()
+                                         .objectFromSource(json, toInstanceOfClass: type) as! Type
+                
+                onSuccess(entity)
+                        
+            } else if let error = error {
+                onError(error)
+            }
+        }
+    }
+    
+    func request <Type: NSObject> (method: Alamofire.Method,
+                                   url: URLStringConvertible,
+                                   parameterMap: [String : AnyObject],
+                                   type: Type.Type,
+                                   onError: (NSError) -> Void,
+                                   onSuccess: (Type) -> Void) {
+                                    
+        Alamofire.request(method, url, parameters: parameterMap, encoding: .JSON)
+                 .validate()
+                 .responseJSON { (request, response, json, error) in
+                    
+            if let json: AnyObject = json {
+                
+                let entity = ObjectMapper.sharedInstance()
+                                         .objectFromSource(json, toInstanceOfClass: type) as! Type
+                
+                onSuccess(entity)
+                        
+            } else if let error = error {
+                onError(error)
+            }
+        }
+    }
+    
+    func request <Type: NSObject> (method: Alamofire.Method,
+                                   url: URLStringConvertible,
+                                   parameterMap: [String : AnyObject],
+                                   encoding: ParameterEncoding,
+                                   type: Type.Type,
+                                   onError: (NSError) -> Void,
+                                   onSuccess: (Type) -> Void) {
+                                    
+        Alamofire.request(method, url, parameters: parameterMap, encoding: encoding)
+                 .validate()
+                 .responseJSON { (request, response, json, error) in
+                    
+            if let json: AnyObject = json {
+                
+                let entity = ObjectMapper.sharedInstance()
+                                         .objectFromSource(json, toInstanceOfClass: type) as! Type
+                
+                onSuccess(entity)
+                        
+            } else if let error = error {
+                onError(error)
+            }
+        }
+    }
 }
