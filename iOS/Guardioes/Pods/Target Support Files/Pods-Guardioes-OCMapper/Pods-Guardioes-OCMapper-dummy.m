81dd789c92701fd5cc246d2af8a1cb58e6f728a2
Igor Morais <mor41s.1gor@gmail.com>
2015-06-27 12:18:16 -0300

    Build fix

---
+++ iOS/Guardioes/Pods/Target Support Files/Pods-Guardioes-OCMapper/Pods-Guardioes-OCMapper-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Guardioes_OCMapper : NSObject
+@end
+@implementation PodsDummy_Pods_Guardioes_OCMapper
+@end
---
+++ iOS/Guardioes/Pods/Target Support Files/Pods-Guardioes/Pods-Guardioes-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GuardioesVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GuardioesVersionString[];
+
