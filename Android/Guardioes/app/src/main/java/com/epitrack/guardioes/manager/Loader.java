9940cc955ed789386bb829508de8cbc0b3e6013d
Igor Morais <mor41s.1gor@gmail.com>
2015-07-25 00:31:51 -0300

    Add Loader

---
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/manager/Loader.java
@@ -0,0 +1,39 @@
+package com.epitrack.guardioes.manager;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+
+import com.epitrack.guardioes.utility.Logger;
+
+public final class Loader {
+
+    private static final String TAG = Loader.class.getSimpleName();
+
+    private Handler handler;
+
+    private final HandlerThread handlerThread = new HandlerThread(TAG);
+
+    private Loader() {
+
+        handlerThread.start();
+
+        Logger.logDebug(TAG, "Start");
+    }
+
+    private static final class LazyLoader {
+        private static final Loader INSTANCE = new Loader();
+    }
+
+    public static Loader with() {
+        return LazyLoader.INSTANCE;
+    }
+
+    public final Handler getHandler() {
+
+        if (handler == null) {
+            handler = new Handler(handlerThread.getLooper());
+        }
+
+        return handler;
+    }
+}
