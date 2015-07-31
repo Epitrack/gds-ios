6c6129ac279316718080c4de995837e897e637ff
Igor Morais <mor41s.1gor@gmail.com>
2015-07-31 01:57:19 -0300

    Some stubs images for notice screen

--- Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/NoticeAdapter.java
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/NoticeAdapter.java
@@ -49,51 +49,57 @@ public class NoticeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
         final Notice news2 = new Notice();
 
-        news2.setTitle("É possivel pegar mais de uma gripe ao mesmo tempo?");
-        news2.setSource("mundoestranho.abril.com.br");
-        news2.setPublicationDate("2 dias atrás");
+        news2.setTitle("Ministério da Saúde convoca população para fazer teste da hepatite C");
+        news2.setSource("agenciabrasil.ebc.com.br/");
+        news2.setPublicationDate("4 dias atrás");
 
         final Notice news3 = new Notice();
 
-        news3.setTitle("É possivel pegar mais de uma gripe ao mesmo tempo?");
-        news3.setSource("mundoestranho.abril.com.br");
-        news3.setPublicationDate("2 dias atrás");
+        news3.setTitle("Vacinação contra gripe é estendida em Bauru e Marília");
+        news3.setSource("g1.globo.com");
+        news3.setPublicationDate("4 dias atrás");
 
         final Notice news4 = new Notice();
 
-        news4.setTitle("É possivel pegar mais de uma gripe ao mesmo tempo?");
-        news4.setSource("mundoestranho.abril.com.br");
-        news4.setPublicationDate("2 dias atrás");
+        news4.setTitle("Gripe aviária se espalha pela África; ONU teme contágio humano");
+        news4.setSource("g1.globo.com");
+        news4.setPublicationDate("5 dias atrás");
 
         final Notice news5 = new Notice();
 
-        news5.setTitle("É possivel pegar mais de uma gripe ao mesmo tempo?");
-        news5.setSource("mundoestranho.abril.com.br");
-        news5.setPublicationDate("2 dias atrás");
+        news5.setTitle("Aluno ganha medalha por remédio contra gripe à base de acerola e caju");
+        news5.setSource("g1.globo.com");
+        news5.setPublicationDate("6 dias atrás");
 
         final Notice news6 = new Notice();
 
-        news6.setTitle("É possivel pegar mais de uma gripe ao mesmo tempo?");
-        news6.setSource("mundoestranho.abril.com.br");
-        news6.setPublicationDate("2 dias atrás");
+        news6.setTitle("Cientistas criam vacina experimental que gera anticorpos do HIV em roedor");
+        news6.setSource("g1.globo.com");
+        news6.setPublicationDate("7 dias atrás");
 
         final Notice news7 = new Notice();
 
-        news7.setTitle("É possivel pegar mais de uma gripe ao mesmo tempo?");
+        news7.setTitle("Qual é a diferença entre gripe e resfriado?");
         news7.setSource("mundoestranho.abril.com.br");
-        news7.setPublicationDate("2 dias atrás");
+        news7.setPublicationDate("8 dias atrás");
 
         final Notice news8 = new Notice();
 
-        news8.setTitle("É possivel pegar mais de uma gripe ao mesmo tempo?");
-        news8.setSource("mundoestranho.abril.com.br");
-        news8.setPublicationDate("2 dias atrás");
+        news8.setTitle("Qual a diferença entre febre amarela, dengue e malária?");
+        news8.setSource("super.abril.com.br");
+        news8.setPublicationDate("10 dias atrás");
 
         final Notice news9 = new Notice();
 
-        news9.setTitle("É possivel pegar mais de uma gripe ao mesmo tempo?");
-        news9.setSource("mundoestranho.abril.com.br");
-        news9.setPublicationDate("2 dias atrás");
+        news9.setTitle("Fio cruz inova em estudo de vacina contra a leishmaniose");
+        news9.setSource("blog.saude.gov.br");
+        news9.setPublicationDate("11 dias atrás");
+
+//        final Notice news10 = new Notice();
+//
+//        news10.setTitle("Coreia do Sul declara extinta a epidemia do novo coronavírus");
+//        news10.setSource("http://noticias.r7.com/");
+//        news10.setPublicationDate("12 dias atrás");
 
         newsList.add(news);
         newsList.add(news1);
@@ -105,6 +111,7 @@ public class NoticeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         newsList.add(news7);
         newsList.add(news8);
         newsList.add(news9);
+        //newsList.add(news10);
     }
 
     public static class ViewType {
@@ -198,7 +205,34 @@ public class NoticeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             itemHolder.textViewTitle.setText(news.getTitle());
             itemHolder.textViewSource.setText(news.getSource());
             itemHolder.textViewDate.setText(news.getPublicationDate());
-            itemHolder.imageViewImage.setImageResource(R.drawable.stub1);
+
+            if (position == 1) {
+                itemHolder.imageViewImage.setImageResource(R.drawable.stub1);
+
+            } else if (position == 2) {
+                itemHolder.imageViewImage.setImageResource(R.drawable.news_1);
+
+            } else if (position == 3) {
+                itemHolder.imageViewImage.setImageResource(R.drawable.news_2);
+
+            } else if (position == 4) {
+                itemHolder.imageViewImage.setImageResource(R.drawable.news_3);
+
+            } else if (position == 5) {
+                itemHolder.imageViewImage.setImageResource(R.drawable.news_4);
+
+            } else if (position == 6) {
+                itemHolder.imageViewImage.setImageResource(R.drawable.news_5);
+
+            } else if (position == 7) {
+                itemHolder.imageViewImage.setImageResource(R.drawable.news_6);
+
+            } else if (position == 8) {
+                itemHolder.imageViewImage.setImageResource(R.drawable.news_7);
+
+            } else if (position == 9) {
+                itemHolder.imageViewImage.setImageResource(R.drawable.news_8);
+            }
         }
     }
 
