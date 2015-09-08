1a3088792ba178fde914d2e5ef6ac2eda2cb8de1
Igor Morais <mor41s.1gor@gmail.com>
2015-09-07 21:54:58 -0300

    Implementing layout in diary of health

--- Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/diary/DiaryActivity.java
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/diary/DiaryActivity.java
@@ -1,19 +1,89 @@
 package com.epitrack.guardioes.view.diary;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import com.epitrack.guardioes.R;
-import com.epitrack.guardioes.view.BaseAppCompatActivity;
+import com.epitrack.guardioes.view.base.BaseAppCompatActivity;
+import com.epitrack.guardioes.view.survey.ParentListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
 
 /**
  * @author Igor Morais
  */
-public class DiaryActivity extends BaseAppCompatActivity {
+public class DiaryActivity extends BaseAppCompatActivity implements ParentListener {
+
+    @Bind(R.id.text_view_participation)
+    TextView textViewParticipation;
+
+    @Bind(R.id.text_view_good_percentage)
+    TextView textViewGoodPercentage;
+
+    @Bind(R.id.text_view_good_report)
+    TextView textViewGoodReport;
+
+    @Bind(R.id.text_view_bad_percentage)
+    TextView textViewBadPercentage;
+
+    @Bind(R.id.text_view_bad_report)
+    TextView textViewBadReport;
+
+    @Bind(R.id.text_view_day)
+    TextView textViewDay;
+
+    @Bind(R.id.text_view_date)
+    TextView textViewDate;
+
+    @Bind(R.id.text_view_symptom)
+    TextView textViewSymptom;
+
+    @Bind(R.id.recycler_view)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(final Bundle bundle) {
         super.onCreate(bundle);
 
         setContentView(R.layout.diary);
+
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        // TODO: Need take real data
+        final List<MemberAdapter.Parent> parentList = new ArrayList<>();
+
+        recyclerView.setAdapter(new MemberAdapter(this, parentList));
+
+        // TODO: Stub
+        setupView();
+    }
+
+    @Override
+    public void onParentSelect() {
+
+    }
+
+    private void setupView() {
+
+        // TODO: Stub
+
+        textViewParticipation.setText("162 Participações");
+
+        textViewGoodPercentage.setText("68% Bem");
+        textViewGoodReport.setText("48.452 Relatórios");
+
+        textViewBadPercentage.setText("32% Mal");
+        textViewBadReport.setText("22.800 Relatórios");
+
+        textViewDay.setText("Sexta-feira");
+        textViewDate.setText("15 de maio");
+        textViewSymptom.setText("7 Sintomas");
     }
 }
--- Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/diary/MemberAdapter.java
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/diary/MemberAdapter.java
@@ -1,7 +1,150 @@
 package com.epitrack.guardioes.view.diary;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.epitrack.guardioes.R;
+import com.epitrack.guardioes.view.survey.ParentListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
- * Created by IgorMorais on 9/7/15.
+ * @author Igor Morais
  */
-public class MemberAdapter {
+public class MemberAdapter extends RecyclerView.Adapter<MemberAdapter.ViewHolder> {
+
+    private final ParentListener listener;
+
+    private List<Parent> parentList = new ArrayList<>();
+
+    public MemberAdapter(final ParentListener listener, final List<Parent> parentList) {
+
+        if (listener == null) {
+            throw new IllegalArgumentException("The listener cannot be null.");
+        }
+
+        this.listener = listener;
+        this.parentList = parentList;
+
+        // TODO: Remove this.. Stub only
+
+        final Parent parent1 = new Parent();
+        parent1.name = "Aninha";
+        parent1.age = 15;
+
+        final Parent parent2 = new Parent();
+        parent2.name = "Gui";
+        parent2.age = 21;
+
+        final Parent parent3 = new Parent();
+        parent3.name = "Peu";
+        parent3.age = 9;
+
+        final Parent parent4 = new Parent();
+        parent4.name = "Cadu";
+        parent4.age = 28;
+
+        final Parent parent5 = new Parent();
+        parent5.name = "Cah";
+        parent5.age = 25;
+
+        final Parent parent6 = new Parent();
+        parent6.name = "Gui";
+        parent6.age = 22;
+
+        final Parent parent7 = new Parent();
+        parent7.name = "Ju";
+        parent7.age = 18;
+
+        parentList.add(parent1);
+        parentList.add(parent2);
+        parentList.add(parent3);
+        parentList.add(parent4);
+        parentList.add(parent5);
+        parentList.add(parent6);
+        parentList.add(parent7);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        @Bind(R.id.text_view_name)
+        TextView textViewName;
+
+        @Bind(R.id.image_view_photo)
+        ImageView imageViewPhoto;
+
+        public ViewHolder(final View view) {
+            super(view);
+
+            ButterKnife.bind(this, view);
+
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(final View view) {
+
+            listener.onParentSelect();
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(final ViewGroup viewGroup, final int viewType) {
+
+        final View view = LayoutInflater.from(viewGroup.getContext())
+                                        .inflate(R.layout.diary_member_item, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder viewHolder, final int position) {
+
+        final Parent parent = parentList.get(position);
+
+        viewHolder.textViewName.setText(parent.name);
+
+        if (position == 0) {
+            viewHolder.imageViewPhoto.setImageResource(R.drawable.image_avatar_1);
+
+        } else if (position == 1) {
+            viewHolder.imageViewPhoto.setImageResource(R.drawable.image_avatar_2);
+
+        } else if (position == 2) {
+            viewHolder.imageViewPhoto.setImageResource(R.drawable.image_avatar_3);
+
+        } else if (position == 3) {
+            viewHolder.imageViewPhoto.setImageResource(R.drawable.image_avatar_4);
+
+        } else if (position == 4) {
+            viewHolder.imageViewPhoto.setImageResource(R.drawable.image_avatar_5);
+
+        } else if (position == 5) {
+            viewHolder.imageViewPhoto.setImageResource(R.drawable.image_avatar_6);
+
+        } else if (position == 6) {
+            viewHolder.imageViewPhoto.setImageResource(R.drawable.image_avatar_7);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return parentList.size();
+    }
+
+    // TODO: Remove this.. Stub only
+
+    public class Parent {
+
+        public String name;
+        public int age;
+    }
 }
