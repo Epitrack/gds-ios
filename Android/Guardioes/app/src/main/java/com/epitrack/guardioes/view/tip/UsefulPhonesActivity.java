bc4fe1a56a02aadf5a24d9e127857cf013600ecd
miqueias <miqueias.lopes@gmail.com>
2015-09-17 13:38:55 -0300

    Screens vaccines, prevention, primary care and useful telephone numbers.

--- Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/tip/UsefulPhonesActivity.java
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/tip/UsefulPhonesActivity.java
@@ -1,20 +1,23 @@
 package com.epitrack.guardioes.view.tip;
 
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.epitrack.guardioes.R;
 import com.epitrack.guardioes.view.base.BaseAppCompatActivity;
-import com.epitrack.guardioes.view.base.BaseFragment;
-import com.epitrack.guardioes.view.base.BaseFragmentActivity;
+
+import butterknife.Bind;
+import butterknife.OnClick;
 
 public class UsefulPhonesActivity extends BaseAppCompatActivity {
+    int phoneId;
+
+    @Bind(R.id.button_call)
+    Button buttonCall;
 
     @Override
     public void onCreate(final Bundle bundle) {
@@ -22,21 +25,40 @@ public class UsefulPhonesActivity extends BaseAppCompatActivity {
 
         setContentView(R.layout.useful_phones);
 
-        int phoneId = Integer.parseInt(getIntent().getStringExtra("phone_id"));
-
-        /*EMERGENCY       (1, R.string.emergency, ""),
-        POLICE          (2, R.string.police, ""),
-        FIREMAN         (3, R.string.fireman, ""),
-        DEFENSE         (4, R.string.defense, "");*/
+        phoneId = Integer.parseInt(getIntent().getStringExtra("phone_id"));
+        ImageView imageView = (ImageView) findViewById(R.id.image_call);
+        TextView textView = (TextView) findViewById(R.id.text_call);
 
         if (phoneId == Phone.EMERGENCY.getId()) {
-
+            imageView.setImageResource(R.drawable.icon_samu);
+            textView.setText("");
         } else if (phoneId == Phone.POLICE.getId()) {
-
+            imageView.setImageResource(R.drawable.icon_police);
+            textView.setText("");
         } else if (phoneId == Phone.FIREMAN.getId()) {
-
+            imageView.setImageResource(R.drawable.icon_firefighter);
+            textView.setText("");
         } else if (phoneId == Phone.DEFENSE.getId()) {
-
+            imageView.setImageResource(R.drawable.icon_civildefense);
+            textView.setText("");
         }
+    }
+
+    @OnClick(R.id.button_call)
+    public void onClick() {
+        Intent itent;
+        Uri uri = null;
+
+        if (phoneId == Phone.EMERGENCY.getId()) {
+            uri = Uri.parse("192");
+        } else if (phoneId == Phone.POLICE.getId()) {
+            uri = Uri.parse("190");
+        } else if (phoneId == Phone.FIREMAN.getId()) {
+            uri = Uri.parse("193");
+        } else if (phoneId == Phone.DEFENSE.getId()) {
+            uri = Uri.parse("08006440199");
+        }
+        itent = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(itent);
     }
 }
