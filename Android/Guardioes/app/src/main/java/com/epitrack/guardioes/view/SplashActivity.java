11b408f25fe565fe606ffbfb5299ffcd58b686ce
miqueiaslopes <miqueias.lopes@gmail.com>
2015-10-31 10:29:37 -0300

    1. Set flow of the registration screen. When the user is registered and logged in, go straight to the home screen and does not display the register screen and enter as it is today.

--- Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/SplashActivity.java
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/SplashActivity.java
@@ -1,14 +1,25 @@
 package com.epitrack.guardioes.view;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import com.epitrack.guardioes.R;
 import com.epitrack.guardioes.manager.PrefManager;
+import com.epitrack.guardioes.model.SingleUser;
+import com.epitrack.guardioes.request.Method;
+import com.epitrack.guardioes.request.Requester;
+import com.epitrack.guardioes.request.SimpleRequester;
 import com.epitrack.guardioes.utility.Constants;
 import com.epitrack.guardioes.view.base.BaseActivity;
 import com.epitrack.guardioes.view.welcome.WelcomeActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author Igor Morais
@@ -18,6 +29,8 @@ public class SplashActivity extends BaseActivity implements Runnable {
     private static final long WAIT_TIME = 1500;
 
     private final Handler handler = new Handler();
+    SharedPreferences sharedPreferences = null;
+
 
     @Override
     protected void onCreate(final Bundle bundle) {
@@ -38,13 +51,67 @@ public class SplashActivity extends BaseActivity implements Runnable {
     @Override
     public void run() {
 
-        if (new PrefManager(this).get(Constants.Pref.USER) == null) {
-            navigateTo(WelcomeActivity.class);
+        sharedPreferences = getSharedPreferences(Constants.Pref.PREFS_NAME, 0);
+        String prefUserToken = sharedPreferences.getString(Constants.Pref.PREFS_NAME, "");
+
+        if (!prefUserToken.equals("")) {
+
+            SingleUser singleUser = SingleUser.getInstance();
+            JSONObject jsonObject = new JSONObject();
+
+            singleUser.setUser_token(prefUserToken);
+
+            SimpleRequester sendPostRequest = new SimpleRequester();
+            sendPostRequest.setUrl(Requester.API_URL + "user/lookup/");
+            sendPostRequest.setJsonObject(jsonObject);
+            sendPostRequest.setMethod(Method.GET);
+
+            String jsonStr;
+            try {
+                jsonStr = sendPostRequest.execute(sendPostRequest).get();
+
+                jsonObject = new JSONObject(jsonStr);
+
+                if (jsonObject.get("error").toString() == "true") {
+                    navigateTo(WelcomeActivity.class);
+                } else {
+
+                    JSONObject jsonObjectUser = jsonObject.getJSONObject("data");
+
+                    singleUser.setNick(jsonObjectUser.getString("nick").toString());
+                    singleUser.setEmail(jsonObjectUser.getString("email").toString());
+                    singleUser.setGender(jsonObjectUser.getString("gender").toString());
+                    singleUser.setId(jsonObjectUser.getString("id").toString());
+                    singleUser.setRace(jsonObjectUser.getString("race").toString());
+                    singleUser.setDob(jsonObjectUser.getString("dob").toString());
+                    singleUser.setUser_token(jsonObjectUser.get("token").toString());
+
+                    try {
+                        singleUser.setPicture(jsonObjectUser.get("picture").toString());
+                    } catch (Exception e) {
+                        singleUser.setPicture("0");
+                    }
+
+                    singleUser.setHashtags(jsonObjectUser.getJSONArray("hashtags"));
+
+                    SharedPreferences settings = getSharedPreferences(Constants.Pref.PREFS_NAME, 0);
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putString(Constants.Pref.PREFS_NAME, singleUser.getUser_token());
+                    editor.commit();
+
+                    navigateTo(HomeActivity.class, Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                            Intent.FLAG_ACTIVITY_NEW_TASK);
+                }
+            } catch (InterruptedException e) {
+                navigateTo(WelcomeActivity.class);
+            } catch (ExecutionException e) {
+                navigateTo(WelcomeActivity.class);
+            } catch (JSONException e) {
+                navigateTo(WelcomeActivity.class);
+            }
 
         } else {
-
-            navigateTo(HomeActivity.class, Intent.FLAG_ACTIVITY_CLEAR_TASK |
-                                           Intent.FLAG_ACTIVITY_NEW_TASK);
+            navigateTo(WelcomeActivity.class);
         }
     }
 }
