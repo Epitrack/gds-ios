bb40a859312e0274088a6b0d1b87f0a84debfd92
Douglas Queiroz <douglasq16@gmail.com>
2016-02-15 17:26:43 -0300

    #113095331 - GdD IOS - Notícias Items Too Small

--- iOS/Guardioes da Saude/NoticeViewController.m
+++ iOS/Guardioes da Saude/NoticeViewController.m
@@ -18,6 +18,7 @@
     SingleNotice *singleNotice;
     NSMutableArray *notices;
 }
+@property (weak, nonatomic) IBOutlet UIImageView *imgHeader;
 
 @end
 
@@ -36,6 +37,10 @@
                                 target:self
                                 action:nil];
     
+    UIView *overlay = [[UIView alloc] initWithFrame:[self.imgHeader frame]];
+    [overlay setBackgroundColor:[UIColor colorWithRed:(10/255.0) green:(88/255.0) blue:(163/255.0) alpha:0.7]];
+    
+    [self.imgHeader addSubview:overlay];
     self.navigationController.navigationBar.topItem.backBarButtonItem = btnBack;
 }
 
