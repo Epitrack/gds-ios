72248dcd47938372f2c2615530b4aa6cf194651e
Douglas Queiroz <douglasq16@gmail.com>
2016-02-29 16:11:13 -0300

    Adjustment profile file name.

--- Guardioes da Saude/SelectAvatarViewController.h
+++ Guardioes da Saude/SelectAvatarViewController.h
@@ -18,37 +18,9 @@
 
 @property UIImagePickerController *picker;
 
-@property (weak, nonatomic) IBOutlet UIButton *btn1;
-@property (weak, nonatomic) IBOutlet UIButton *btn2;
 @property (weak, nonatomic) IBOutlet UIButton *btnPhoto;
-@property (weak, nonatomic) IBOutlet UIButton *btn3;
-@property (weak, nonatomic) IBOutlet UIButton *btn4;
-@property (weak, nonatomic) IBOutlet UIButton *btn5;
-@property (weak, nonatomic) IBOutlet UIButton *btn6;
-@property (weak, nonatomic) IBOutlet UIButton *btn7;
-@property (weak, nonatomic) IBOutlet UIButton *btn8;
-@property (weak, nonatomic) IBOutlet UIButton *btn9;
-@property (weak, nonatomic) IBOutlet UIButton *btn10;
-@property (weak, nonatomic) IBOutlet UIButton *btn11;
-@property (weak, nonatomic) IBOutlet UIButton *btn12;
-- (IBAction)btnPhotoAction:(id)sender;
-- (IBAction)btn1Action:(id)sender;
-- (IBAction)btn2Action:(id)sender;
-- (IBAction)btn5Action:(id)sender;
-- (IBAction)btn6Action:(id)sender;
-- (IBAction)btn3Action:(id)sender;
-- (IBAction)btn4Action:(id)sender;
-- (IBAction)btn8Action:(id)sender;
-- (IBAction)btn9Action:(id)sender;
-- (IBAction)btn10Action:(id)sender;
-- (IBAction)btn7Action:(id)sender;
-- (IBAction)btn11Action:(id)sender;
-- (IBAction)btn12Action:(id)sender;
-- (IBAction)btn13Action:(id)sender;
-- (IBAction)btn14Action:(id)sender;
-- (IBAction)btn15Action:(id)sender;
-- (IBAction)btn16Action:(id)sender;
-- (IBAction)btn17Action:(id)sender;
+
+- (IBAction)selectAvatarAction:(id)sender;
 
 -(void) setUrlPhoto: (NSString *) urlPhoto;
 
--- Guardioes da Saude/SelectAvatarViewController.m
+++ Guardioes da Saude/SelectAvatarViewController.m
@@ -176,6 +176,11 @@
     [picker dismissViewControllerAnimated:YES completion:NULL];
 }
 
+- (IBAction)selectAvatarAction:(id)sender {
+    NSInteger tag = ((UIButton *) sender).tag;
+    [self setAvatar:[NSNumber numberWithInteger:tag]];
+}
+
 -(void)setUrlPhoto:(NSString *)urlPhoto{
     [self.profileFormCtr setPhoto:urlPhoto];
     [self.navigationController popViewControllerAnimated:YES];
@@ -231,74 +236,6 @@
     CGImageRelease(imageRef);
     
     return cropped;
-}
-
-- (IBAction)btn1Action:(id)sender {
-    [self setAvatar:@1];
-}
-
-- (IBAction)btn2Action:(id)sender {
-    [self setAvatar:@2];
-}
-
-- (IBAction)btn5Action:(id)sender {
-    [self setAvatar:@5];
-}
-
-- (IBAction)btn6Action:(id)sender {
-    [self setAvatar:@6];
-}
-
-- (IBAction)btn4Action:(id)sender {
-    [self setAvatar:@4];
-}
-
-- (IBAction)btn8Action:(id)sender {
-    [self setAvatar:@8];
-}
-
-- (IBAction)btn9Action:(id)sender {
-    [self setAvatar:@9];
-}
-
-- (IBAction)btn10Action:(id)sender {
-    [self setAvatar:@10];
-}
-
-- (IBAction)btn7Action:(id)sender {
-    [self setAvatar:@7];
-}
-
-- (IBAction)btn11Action:(id)sender {
-    [self setAvatar:@11];
-}
-
-- (IBAction)btn12Action:(id)sender {
-    [self setAvatar:@12];
-}
-
-- (IBAction)btn13Action:(id)sender {
-    [self setAvatar:@13];
-}
-
-- (IBAction)btn14Action:(id)sender {
-    [self setAvatar:@14];
-}
-
-- (IBAction)btn15Action:(id)sender {
-    [self setAvatar:@15];
-}
-
-- (IBAction)btn16Action:(id)sender {
-    [self setAvatar:@16];
-}
-
-- (IBAction)btn17Action:(id)sender {
-    [self setAvatar:@17];
-}
-
-- (IBAction)btn3Action:(id)sender {
-    [self setAvatar:@3];
 }
 
 - (void) setAvatar:(NSNumber *)idAvatar {
