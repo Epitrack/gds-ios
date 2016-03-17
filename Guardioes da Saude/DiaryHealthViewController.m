982edac984733440667b4981761c4529950dc6cf
Douglas Queiroz <douglasq16@gmail.com>
2016-03-17 14:08:54 -0300

    #115061149 - Dev iOs Routine: review

--- Guardioes da Saude/DiaryHealthViewController.m
+++ Guardioes da Saude/DiaryHealthViewController.m
@@ -587,7 +587,7 @@ const float _kCellHeight = 100.0f;
 
 - (UIImageView *) getState: (SumaryCalendar *) sumaryCalendar {
     
-    UIImageView * view = [[UIImageView alloc] initWithFrame: CGRectMake(9, 0, 23, 23)];
+    UIImageView * view = [[UIImageView alloc] initWithFrame: CGRectMake(self.calendarContentView.bounds.size.width/29, 0, 25, 24)];
     
     if (sumaryCalendar.noSymptomAmount == 0) {
         
