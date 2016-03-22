cefb93fb97304d6f0e093c1703bdf43d011f62d7
Douglas Queiroz <douglasq16@gmail.com>
2016-03-22 12:54:18 -0300

    #112422873: GDS IOS: Export Strings

---
+++ Guardioes da Saude/GDSButton.m
@@ -0,0 +1,30 @@
+//
+//  GDSButton.m
+//  Guardioes da Saude
+//
+//  Created by Douglas Queiroz on 3/22/16.
+//  Copyright © 2016 epitrack. All rights reserved.
+//
+
+#import "GDSButton.h"
+
+@implementation GDSButton
+
+- (void)drawRect:(CGRect)rect {
+    NSString *text = NSLocalizedStringWithDefaultValue([self titleLabel].text, nil, [NSBundle mainBundle], [self titleLabel].text, @"");
+    
+    if (self.titleLabel.attributedText && [[self titleLabel].text length] > 0) {
+        NSDictionary *attributes = [(NSAttributedString *) self.titleLabel.attributedText attributesAtIndex:0 effectiveRange:NULL];
+        
+        NSAttributedString *title = [[NSAttributedString alloc] initWithString:text attributes:attributes];
+        
+        [self setAttributedTitle:title forState:UIControlStateNormal];
+    }else{
+        [self setTitle:text forState:UIControlStateNormal];
+    }
+    
+    [super drawRect:rect];
+}
+
+
+@end
