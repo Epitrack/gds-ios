fce79fd2011929f0255778153cfba58c2b3f107d
Douglas Queiroz <douglasq16@gmail.com>
2016-04-29 18:47:54 -0300

    Congratulations game screen

---
+++ Guardioes da Saude/game/CongratulationsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CongratulationsViewController.swift
+//  Guardioes da Saude
+//
+//  Created by Douglas Queiroz on 4/29/16.
+//  Copyright © 2016 epitrack. All rights reserved.
+//
+
+import UIKit
+
+class CongratulationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
