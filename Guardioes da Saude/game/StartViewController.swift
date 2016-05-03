1b018027620fab3253b9526b93a29a581081e821
Douglas Queiroz <douglasq16@gmail.com>
2016-05-03 17:03:06 -0300

    Adjustment Auto-layout to iPhone 6 at start screen of the game.

--- Guardioes da Saude/game/StartViewController.swift
+++ Guardioes da Saude/game/StartViewController.swift
@@ -43,48 +43,19 @@ class StartViewController: UIViewController {
             }
         }
         
-        // Use UIBezierPath as an easy way to create the CGPath for the layer.
-        // The path should be the entire circle.
-        let circlePath = UIBezierPath(arcCenter: CGPoint(x: self.viewQuestionTimer.frame.size.width / 2.0, y: self.viewQuestionTimer.frame.size.height / 2.0), radius: (self.viewQuestionTimer.frame.size.width - 10)/2, startAngle: 0.0, endAngle: CGFloat(M_PI * 2.0), clockwise: true)
-        
-        // Setup the CAShapeLayer with the path, colors, and line width
         circleLayer = CAShapeLayer()
-        circleLayer.path = circlePath.CGPath
-        circleLayer.fillColor = UIColor.clearColor().CGColor
-        circleLayer.strokeColor = UIColor(red: 239.0/255.0, green: 98.0/255.0, blue: 26.0/255.0, alpha: 1).CGColor
-        circleLayer.lineWidth = 7.0;
         
-        // Don't draw the circle initially
-        circleLayer.strokeEnd = 0.0
-        
-        
-        // Add the circleLayer to the view's layer's sublayers
-        self.viewQuestionTimer.layer.addSublayer(circleLayer)
-        self.viewQuestionTimer.layer.cornerRadius = self.viewQuestionTimer.frame.width/2;
     }
     
     func animateCircle(duration: NSTimeInterval) {
-        // We want to animate the strokeEnd property of the circleLayer
         let animation = CABasicAnimation(keyPath: "strokeEnd")
-        
-        // Set the animation duration appropriately
         animation.duration = duration
-        
-        // Animate from 0 (no circle) to 1 (full circle)
         animation.fromValue = 0
         animation.toValue = 1
-        
-        // Do a linear animation (i.e. the speed of the animation stays the same)
         animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
         
-        // Set the circleLayer's strokeEnd property to 1.0 now so that it's the
-        // right value when the animation ends.
         circleLayer.strokeEnd = 1.0
-        
-        // Do the actual animation
         circleLayer.addAnimation(animation, forKey: "animateCircle")
-        
-        
         
         let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
         dispatch_async(queue, {
@@ -117,6 +88,23 @@ class StartViewController: UIViewController {
             
             self.navigationController?.navigationBar.addSubview(self.titleBarImage!)
         }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: self.viewQuestionTimer.frame.size.width / 2.0, y: self.viewQuestionTimer.frame.size.height / 2.0), radius: (self.viewQuestionTimer.frame.size.width - 10)/2, startAngle: 0.0, endAngle: CGFloat(M_PI * 2.0), clockwise: true)
+        
+        circleLayer.path = circlePath.CGPath
+        circleLayer.fillColor = UIColor.clearColor().CGColor
+        circleLayer.strokeColor = UIColor(red: 239.0/255.0, green: 98.0/255.0, blue: 26.0/255.0, alpha: 1).CGColor
+        circleLayer.lineWidth = 7.0;
+        
+        // Don't draw the circle initially
+        circleLayer.strokeEnd = 0.0
+        
+        // Add the circleLayer to the view's layer's sublayers
+        self.viewQuestionTimer.layer.addSublayer(circleLayer)
+        self.viewQuestionTimer.layer.cornerRadius = self.viewQuestionTimer.frame.width/2;
+
     }
     
     override func viewWillDisappear(animated: Bool) {
