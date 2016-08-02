0849b0fc476697467a42826fe7a68d8229b589b4
Douglas Queiroz <douglasq16@gmail.com>
2016-08-02 16:44:56 -0300

    #127491177 - Gds iOS - Wrong point at the GDS Game

--- Guardioes da Saude/game/QuestionViewController.swift
+++ Guardioes da Saude/game/QuestionViewController.swift
@@ -166,7 +166,6 @@ class QuestionViewController: UIViewController {
             }
             dispatch_async(dispatch_get_main_queue()) {
                 self.puzzeViewCtrlRef.closeQuestionDialog()
-                self.puzzeViewCtrlRef.startViewRef.reducePoint()
             }
         })
     }
